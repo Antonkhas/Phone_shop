d13a52db45dd9df3f7e2950e091672ca1f284bf2
antonkhas <toha1399@mail.ru>
2023-09-20 17:40:55 +0300

    добавил router

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,12 @@
+
 function Product() {
     return (
-        <div>
-            Товары и тп
-        </div>
-    )
+    <>
+        <header className=" d-flex justify-center p-20">
+            <a href="/" >Главная</a>
+        </header>
+    </>
+    );
 }
 
 export default Product
